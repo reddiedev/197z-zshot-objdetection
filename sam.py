29e7a871caed04dd58ce6b205a7b374eb0795cc1
reddiedev <105657266+reddiedev@users.noreply.github.com>
2023-05-13 19:48:19 +0800

    first commit

---
+++ sam.py
@@ -0,0 +1,126 @@
+
+import sys
+import torch
+import torchvision
+print("PyTorch version:", torch.__version__)
+print("Torchvision version:", torchvision.__version__)
+print("CUDA is available:", torch.cuda.is_available())
+
+import numpy as np
+import torch
+import matplotlib.pyplot as plt
+import cv2
+
+def show_mask(mask, ax, random_color=False):
+    if random_color:
+        color = np.concatenate([np.random.random(3), np.array([0.6])], axis=0)
+    else:
+        color = np.array([30/255, 144/255, 255/255, 0.6])
+    h, w = mask.shape[-2:]
+    mask_image = mask.reshape(h, w, 1) * color.reshape(1, 1, -1)
+    ax.imshow(mask_image)
+    
+def show_points(coords, labels, ax, marker_size=375):
+    pos_points = coords[labels==1]
+    neg_points = coords[labels==0]
+    ax.scatter(pos_points[:, 0], pos_points[:, 1], color='green', marker='*', s=marker_size, edgecolor='white', linewidth=1.25)
+    ax.scatter(neg_points[:, 0], neg_points[:, 1], color='red', marker='*', s=marker_size, edgecolor='white', linewidth=1.25)   
+    
+def show_box(box, ax):
+    x0, y0 = box[0], box[1]
+    w, h = box[2] - box[0], box[3] - box[1]
+    ax.add_patch(plt.Rectangle((x0, y0), w, h, edgecolor='green', facecolor=(0,0,0,0), lw=2))    
+
+image = cv2.imread('images/dog_car.jpg', 1)
+image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+
+plt.figure(figsize=(10,10))
+plt.imshow(image)
+plt.show()
+
+import sys
+import os
+sys.path.append("..")
+from segment_anything import sam_model_registry, SamPredictor
+
+sam_checkpoint = os.path.join("checkpoints", "sam_vit_b_01ec64.pth")
+
+model_type = "vit_b"
+
+device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
+
+sam = sam_model_registry[model_type](checkpoint=sam_checkpoint)
+sam.to(device=device)
+
+predictor = SamPredictor(sam)
+
+predictor.set_image(image)
+
+input_point = np.array([[500, 1000]])
+input_label = np.array([1])
+
+plt.figure(figsize=(10,10))
+plt.imshow(image)
+show_points(input_point, input_label, plt.gca())
+plt.axis('on')
+plt.show()  
+
+masks, scores, logits = predictor.predict(
+    point_coords=input_point,
+    point_labels=input_label,
+    multimask_output=True,
+)
+
+masks.shape  # (number_of_masks) x H x W
+
+
+for i, (mask, score) in enumerate(zip(masks, scores)):
+    plt.figure(figsize=(10,10))
+    plt.imshow(image)
+    show_mask(mask, plt.gca())
+    show_points(input_point, input_label, plt.gca())
+    plt.title(f"Mask {i+1}, Score: {score:.3f}", fontsize=18)
+    plt.axis('off')
+    plt.show()  
+  
+
+input_point = np.array([[500, 1000], [1200, 750]])
+input_label = np.array([1, 1])
+
+mask_input = logits[np.argmax(scores), :, :]  # Choose the model's best mask
+
+masks, _, _ = predictor.predict(
+    point_coords=input_point,
+    point_labels=input_label,
+    mask_input=mask_input[None, :, :],
+    multimask_output=False,
+)
+
+
+plt.figure(figsize=(10,10))
+plt.imshow(image)
+show_mask(masks, plt.gca())
+show_points(input_point, input_label, plt.gca())
+plt.axis('off')
+plt.show() 
+
+input_point = np.array([[500, 1000], [1200, 750]])
+input_label = np.array([1, 0])
+
+mask_input = logits[np.argmax(scores), :, :]  # Choose the model's best mask
+
+masks, _, _ = predictor.predict(
+    point_coords=input_point,
+    point_labels=input_label,
+    mask_input=mask_input[None, :, :],
+    multimask_output=False,
+)
+
+plt.figure(figsize=(10, 10))
+plt.imshow(image)
+show_mask(masks, plt.gca())
+show_points(input_point, input_label, plt.gca())
+plt.axis('off')
+plt.show() 
+
+plt.savefig("output-1.jpg")
---
+++ test.py
@@ -0,0 +1,6 @@
+import sys
+import torch
+import torchvision
+print("PyTorch version:", torch.__version__)
+print("Torchvision version:", torchvision.__version__)
+print("CUDA is available:", torch.cuda.is_available())
