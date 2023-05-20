2412466ff8fd5a5a73a506444e79141d8565ec6a
reddiedev <105657266+reddiedev@users.noreply.github.com>
2023-05-20 21:00:44 +0000

    generating labels using open clip

--- scripts/identify-one.py
+++ scripts/identify-one.py
@@ -1,6 +1,26 @@
 # obtain link to image
 from pycocotools.coco import COCO
 import numpy as np
+import torch
+import torchvision
+from skimage import io
+import matplotlib.pyplot as plt
+from segment_anything import sam_model_registry, SamAutomaticMaskGenerator
+import os
+import open_clip
+import urllib 
+from PIL import Image
+import os
+import glob
+
+print("PyTorch version:", torch.__version__)
+print("Torchvision version:", torchvision.__version__)
+print("CUDA is available:", torch.cuda.is_available())
+
+## clean output directory
+files = glob.glob('../output/*.jpg')
+for f in files:
+    os.remove(f)
 
 print("[0]: loading coco annotations...")
 
@@ -13,7 +33,7 @@ coco = COCO(annFile)
 cats = coco.loadCats(coco.getCatIds())
 nms = [cat['name'] for cat in cats]
 # print('COCO categories: \n{}\n'.format(' '.join(nms)))
-nms = set([cat['supercategory'] for cat in cats])
+# nms = set([cat['supercategory'] for cat in cats])
 # print('COCO supercategories: \n{}'.format(' '.join(nms)))
 
 # get all category IDs
@@ -22,17 +42,21 @@ catIds = coco.getCatIds()
 catId = catIds[np.random.randint(0,len(catIds))]
 # choose random image from category
 imgIds = coco.getImgIds(catIds=[catId] )
-img = coco.loadImgs(imgIds[np.random.randint(0,len(imgIds))])[0]
+imgID = imgIds[np.random.randint(0,len(imgIds))]
+img = coco.loadImgs(imgID)[0]
 imgUrl = img['coco_url']
+
+
+print(f"[0]: generating labels for source image...")
+annFile = '{}/annotations/captions_{}.json'.format(dataDir,dataType)
+coco_caps=COCO(annFile)
+annIds = coco_caps.getAnnIds(imgIds=imgID)
+anns = coco_caps.loadAnns(annIds)
+coco_caps.showAnns(anns)
 
 print(f"[1]: generating image masks for {imgUrl}")
 
 # automatically generate masks using SAM
-import torch
-import torchvision
-print("PyTorch version:", torch.__version__)
-print("Torchvision version:", torchvision.__version__)
-print("CUDA is available:", torch.cuda.is_available())
 
 
 # crop source image to each mask
@@ -52,39 +76,22 @@ def show_anns(anns):
             img[:,:,i] = color_mask[i]
         ax.imshow(np.dstack((img, m*0.35)))
 
-from skimage import io
-import matplotlib.pyplot as plt
 
 image = io.imread(imgUrl)
 
+print(f"[1]: saving source image")
 plt.figure(figsize=(20,20))
 plt.imshow(image)
 plt.axis('off')
-print(f"[1]: saving source image")
-plt.savefig("../output/source.jpg")
+plt.savefig("../output/source.jpg",bbox_inches='tight',pad_inches = 0)
 
-from segment_anything import sam_model_registry, SamAutomaticMaskGenerator
-import os
-
-sam_checkpoint = os.path.join("../checkpoints", "sam_vit_b_01ec64.pth")
-model_type = "vit_b"
-device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+sam_checkpoint = os.path.join("../checkpoints", "sam_vit_h_4b8939.pth") # sam_vit_b_01ec64 | sam_vit_h_4b8939
+model_type = "vit_h"
+device = torch.device   ('cuda' if torch.cuda.is_available() else 'cpu')
 sam = sam_model_registry[model_type](checkpoint=sam_checkpoint)
 sam.to(device=device)
-mask_generator = SamAutomaticMaskGenerator(sam)
 print(f"[1]: generating masks")
-masks = mask_generator.generate(image)
-print(len(masks))
-print(masks[0].keys())
-
-plt.figure(figsize=(20,20))
-plt.imshow(image)
-show_anns(masks)
-plt.axis('off')
-print(f"[1]: saving masks image")
-plt.savefig("../output/generated-masks.jpg")
-
-mask_generator_2 = SamAutomaticMaskGenerator(
+mask_generator = SamAutomaticMaskGenerator(
     model=sam,
     points_per_side=32,
     pred_iou_thresh=0.86,
@@ -93,19 +100,74 @@ mask_generator_2 = SamAutomaticMaskGenerator(
     crop_n_points_downscale_factor=2,
     min_mask_region_area=100,  # Requires open-cv to run post-processing
 )
-print(f"[1]: generating masks-2")
-masks2 = mask_generator_2.generate(image)
-len(masks2)
-print(masks2[0].keys())
+masks = mask_generator.generate(image)
+print(f"[1]: generated {len(masks)} masks for source image")
 plt.figure(figsize=(20,20))
 plt.imshow(image)
-show_anns(masks2)
+show_anns(masks)
 plt.axis('off')
-print(f"[1]: saving masks-2 image")
-plt.savefig("../output/generated-masks-2.jpg")
+plt.savefig("../output/generated-masks.jpg",bbox_inches='tight',pad_inches = 0)
 
 
 # for each mask image, annotate using open-clip
+
+
+print("[2]: creating open clip model...")
+model, _, preprocess = open_clip.create_model_and_transforms('ViT-B-32', pretrained='laion2b_s34b_b79k')
+tokenizer = open_clip.get_tokenizer('ViT-B-32')
+
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+model.to(device)
+
+print("[2]: loading imagenet 1k labels")
+# load imagenet 1k labels and tokenize them
+filename = "imagenet1000_labels.txt"
+url = "https://gist.githubusercontent.com/yrevar/942d3a0ac09ec9e5eb3a/raw/238f720ff059c1f82f368259d1ca4ffa5dd8f9f5/imagenet1000_clsidx_to_labels.txt"
+
+# Download the file if it does not exist
+if not os.path.isfile(filename):
+    urllib.request.urlretrieve(url, filename)
+
+with open(filename) as f:
+    idx2label = eval(f.read())
+
+imagenet_labels = list(idx2label.values())
+text = tokenizer(imagenet_labels)
+text = text.to(device)
+
+def generate_labels(anns):
+    if len(anns) == 0:
+        return
+    length = len(anns)
+    labels = []
+    for i in range(length):
+        mask = anns[i]
+        x,y,w,h = mask['bbox']
+        im = image[y:y+h, x:x+w]
+        plt.figure(figsize=(20,20))
+        plt.imshow(im)
+        plt.axis('off')
+        plt.savefig(f"../output/mask-{i}.jpg", bbox_inches='tight', pad_inches = 0)
+        ima = Image.open(f"../output/mask-{i}.jpg")
+        processedImage = preprocess(ima).unsqueeze(0)
+        processedImage = processedImage.to(device)
+
+        with torch.no_grad(), torch.cuda.amp.autocast():
+            image_features = model.encode_image(processedImage)
+            text_features = model.encode_text(text)
+            image_features /= image_features.norm(dim=-1, keepdim=True)
+            text_features /= text_features.norm(dim=-1, keepdim=True)
+            text_probs = (100.0 * image_features @ text_features.T).softmax(dim=-1)
+
+
+        index = np.argmax(text_probs.cpu().numpy())
+        label = imagenet_labels[index]
+        labels.append(label)
+        print(label) 
+    return labels
+        
+labels = generate_labels(masks)
+print(labels)
 
 
 # evaluate generated annotations
