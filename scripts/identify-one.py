66c7e3bb6358702416bad92597db59112e1b0d0b
reddiedev <105657266+reddiedev@users.noreply.github.com>
2023-05-21 07:50:20 +0000

    generated code for computing mAP

--- scripts/identify-one.py
+++ scripts/identify-one.py
@@ -1,5 +1,6 @@
 # obtain link to image
 from pycocotools.coco import COCO
+from pycocotools.cocoeval import COCOeval
 import numpy as np
 import torch
 import torchvision
@@ -8,10 +9,12 @@ import matplotlib.pyplot as plt
 from segment_anything import sam_model_registry, SamAutomaticMaskGenerator
 import os
 import open_clip
-import urllib 
 from PIL import Image
 import os
 import glob
+from pprint import pprint
+import json
+
 
 print("PyTorch version:", torch.__version__)
 print("Torchvision version:", torchvision.__version__)
@@ -27,38 +30,37 @@ print("[0]: loading coco annotations...")
 dataDir='../coco'
 dataType='val2017'
 annFile='{}/annotations/instances_{}.json'.format(dataDir,dataType)
-# initialize COCO api for instance annotations
-coco = COCO(annFile)
-# display COCO categories and supercategories
-cats = coco.loadCats(coco.getCatIds())
-nms = [cat['name'] for cat in cats]
-# print('COCO categories: \n{}\n'.format(' '.join(nms)))
-# nms = set([cat['supercategory'] for cat in cats])
-# print('COCO supercategories: \n{}'.format(' '.join(nms)))
+# initialize COCO api for instance annotations + COCO ground truth api
+cocoGt = COCO(annFile)
 
 # get all category IDs
-catIds = coco.getCatIds()
+catIds = cocoGt.getCatIds()
 # choose random category ID
-catId = catIds[np.random.randint(0,len(catIds))]
+catID = catIds[np.random.randint(0,len(catIds))]
 # choose random image from category
-imgIds = coco.getImgIds(catIds=[catId] )
+imgIds = cocoGt.getImgIds(catIds=[catID] )
 imgID = imgIds[np.random.randint(0,len(imgIds))]
-img = coco.loadImgs(imgID)[0]
+img = cocoGt.loadImgs(imgID)[0]
 imgUrl = img['coco_url']
-
+print("ORIGINAL IMAGE")
+pprint(img)
 
 print(f"[0]: generating labels for source image...")
 annFile = '{}/annotations/captions_{}.json'.format(dataDir,dataType)
 coco_caps=COCO(annFile)
 annIds = coco_caps.getAnnIds(imgIds=imgID)
 anns = coco_caps.loadAnns(annIds)
+# print("GROUND TRUTH ANNOTATIONS")
+# pprint(anns)
+ground_truth_labels = []
 ground_truth = []
 for ann in anns:
+    ground_truth_labels.append(ann['caption'])
     words = ann['caption'].split()
     for word in words:
         ground_truth.append(word.lower())
 
-ground_truth = list(set(ground_truth))
+ground_truth_values = list(set(ground_truth))
 
 print(f"[1]: generating image masks for {imgUrl}")
 
@@ -90,8 +92,8 @@ plt.savefig("../output/source.jpg",bbox_inches='tight',pad_inches = 0)
 plt.close()
 
 print("[1]: loading sam model")
-sam_checkpoint = os.path.join("../checkpoints", "sam_vit_b_01ec64.pth") # sam_vit_b_01ec64 | sam_vit_h_4b8939
-model_type = "vit_b"
+sam_checkpoint = os.path.join("../checkpoints", "sam_vit_h_4b8939.pth") # sam_vit_b_01ec64 | sam_vit_h_4b8939
+model_type = "vit_h"
 device = torch.device   ('cuda' if torch.cuda.is_available() else 'cpu')
 sam = sam_model_registry[model_type](checkpoint=sam_checkpoint)
 sam.to(device=device)
@@ -120,27 +122,13 @@ plt.close()
 
 
 print("[2]: creating open clip model...")
-model, _, preprocess = open_clip.create_model_and_transforms('ViT-B-32-quickgelu', pretrained='laion400m_e32')
-tokenizer = open_clip.get_tokenizer('ViT-B-32-quickgelu')
-
-device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+model, _, transform = open_clip.create_model_and_transforms(
+  model_name="coca_ViT-L-14",
+  pretrained="mscoco_finetuned_laion2B-s13B-b90k"
+)
 model.to(device)
 
-print("[2]: loading imagenet 1k labels")
-# load imagenet 1k labels and tokenize them
-filename = "imagenet1000_labels.txt"
-url = "https://gist.githubusercontent.com/yrevar/942d3a0ac09ec9e5eb3a/raw/238f720ff059c1f82f368259d1ca4ffa5dd8f9f5/imagenet1000_clsidx_to_labels.txt"
-
-# Download the file if it does not exist
-if not os.path.isfile(filename):
-    urllib.request.urlretrieve(url, filename)
-
-with open(filename) as f:
-    idx2label = eval(f.read())
-
-imagenet_labels = list(idx2label.values())
-text = tokenizer(imagenet_labels)
-text = text.to(device)
+results = []
 
 def generate_labels(anns):
     if len(anns) == 0:
@@ -150,33 +138,44 @@ def generate_labels(anns):
     for i in range(length):
         mask = anns[i]
         x,y,w,h = mask['bbox']
+        x,y,w,h = int(x), int(y), int(w), int(h)
         im = image[y:y+h, x:x+w]
         plt.figure(figsize=(20,20))
         plt.imshow(im)
         plt.axis('off')
         plt.savefig(f"../output/mask-{i}.jpg", bbox_inches='tight', pad_inches = 0)
         plt.close()
-        ima = Image.open(f"../output/mask-{i}.jpg")
-        processedImage = preprocess(ima).unsqueeze(0)
-        processedImage = processedImage.to(device)
+        im = Image.open(f"../output/mask-{i}.jpg").convert("RGB")
+        im = transform(im).unsqueeze(0)
+        im = im.to(device)
 
         with torch.no_grad(), torch.cuda.amp.autocast():
-            image_features = model.encode_image(processedImage)
-            text_features = model.encode_text(text)
-            image_features /= image_features.norm(dim=-1, keepdim=True)
-            text_features /= text_features.norm(dim=-1, keepdim=True)
-            text_probs = (100.0 * image_features @ text_features.T).softmax(dim=-1)
+            generated = model.generate(im)
 
-
-        index = np.argmax(text_probs.cpu().numpy())
-        label = imagenet_labels[index]
-        if (label in nms):
-            labels.append(label)
-        print(f"[label{i}]: {label} = {label in nms}") 
+        label = open_clip.decode(generated[0]).split("<end_of_text>")[0].replace("<start_of_text>", "")
+        print(f"[{i}]:",mask['predicted_iou'],mask['stability_score'], label)
+        labels.append(label)
+        result = {'image_id':imgID,'category_id':catID,"bbox":mask['bbox'], "score": mask['stability_score']}
+        results.append(result)
     return labels
         
-labels = generate_labels(masks)
-print(labels)
-print(ground_truth)
+generated_labels = generate_labels(masks)
+print("GENERATED LABELS")
+pprint(generated_labels)
+print("GROUND TRUTH LABELS")
+pprint(ground_truth_labels)
 
 # evaluate generated annotations
+
+
+# running evaluation
+# Serializing json
+json_object = json.dumps(results, indent=4)
+with open("../output/results.json", "w") as outfile:
+    outfile.write(json_object)
+cocoDt=cocoGt.loadRes("../output/results.json")
+cocoEval = COCOeval(cocoGt,cocoDt,'bbox')
+cocoEval.params.imgIds  = [imgID]
+cocoEval.evaluate()
+cocoEval.accumulate()
+cocoEval.summarize()
